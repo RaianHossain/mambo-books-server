32e86c64be10431d25012b1420fd617372bda746
RaianHossain <soul.programmer95@gmail.com>
2021-07-01 03:17:55 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require('express')
+const app = express()
+var cors = require('cors')
+const bodyParser = require('body-parser');
+const port = 5002
+app.use(cors())
+app.use(bodyParser.json());
+require('dotenv').config();
+
+const uri = process.env.uri
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+const MongoClient = require('mongodb').MongoClient;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("mambo-books").collection("books");
+  const collection1 = client.db("mambo-books").collection("orders");
+  app.post('/addBook', (req, res) => {
+    const newBook = req.body;
+    console.log(newBook);
+    collection.insertOne(newBook)
+    .then(res => console.log(res.insertedCount))
+    res.send(res.insertedCount > 0)
+})
+
+app.get('/books', (req, res) =>{
+  collection.find({})
+  .toArray((err, documents) => {
+    res.send(documents);
+  })
+})
+
+app.post('/placeOrder', (req, res) => {
+  const newOrder = req.body;
+  console.log(newOrder);
+  collection1.insertOne(newOrder)
+  .then(res => console.log(res.insertedCount))
+  res.send(res.insertedCount>0)
+})
+
+app.get('/orderDetails/:email', (req, res) => {
+  console.log(req.params.email);
+  collection1.find({buyer: req.params.email})  
+  .toArray((err, documents) => {
+    res.send(documents);
+  })
+})
+  // perform actions on the collection object
+});
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
+
+
